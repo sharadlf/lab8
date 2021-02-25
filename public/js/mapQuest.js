ec0bef0d797ad00aa9551289897ff1728ff105eb
Sharad Bhagwat <sharadLF@gmail.com>
2021-02-25 12:22:52 -0800

    completed lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+  L.mapquest.key = '5MAnruSZneH1xYez6AOit1Q6mhQ9hJcu';
+
+// 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map('map', {
+    center: [32.87862840481292, -117.23517789881141],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  // Add marker to map
+  L.marker([32.88, -117.236]).addTo(map);
 }
